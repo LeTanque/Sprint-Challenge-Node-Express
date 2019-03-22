c9546945a25d1641d20ebdd441ba246ab9e99d07
Frank Martinez <frankie.martinez@gmail.com>
2019-03-22 11:26:50 -0700

    basic structure done. Projects component created. Pulling in data now

---
+++ client/src/components/Projects.jsx
@@ -0,0 +1,19 @@
+import React, { Fragment, Component } from 'react';
+
+
+
+class Projects extends Component {
+
+
+  render() {
+    return (
+      <Fragment>
+          <h3>Projects</h3>
+
+
+      </Fragment>
+    );
+  }
+}
+
+export default Projects;
